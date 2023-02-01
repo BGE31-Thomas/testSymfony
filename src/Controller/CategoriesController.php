cf11e10c8a410916a1e1d42ec78fc47bf5af17bf
BGE31-Thomas <tpjarles@gmail.com>
2023-02-01 18:22:44 +0100

    ajout pagination

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 use App\Entity\Categories;
+use App\Repository\ProductsRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -12,9 +14,11 @@ class CategoriesController extends AbstractController
 {
     
     #[Route('/{slug}', name: 'list')]
-    public function list(Categories $category): Response
+    public function list(Categories $category,ProductsRepository $productsRepository,Request $request): Response
     {
-        $products = $category->getProducts();
+        $page = $request->query->getInt('page',1);
+        
+        $products = $productsRepository->findProductsPaginated($page,$category->getSlug(),4);
         
         return $this->render('categories/index.html.twig', compact('category','products'));
     }
